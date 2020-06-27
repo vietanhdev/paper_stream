5831aff1a08598010cbd7f67e8b2765606ff08aa
nguyensyan <syan224@gmail.com>
2020-06-27 17:43:22 +0700

    update hand remover

--- libs/hand_remover/hand_remover.py
+++ libs/hand_remover/hand_remover.py
@@ -3,7 +3,64 @@ from collections import Counter
 import cv2
 import numpy as np
 from threading import Thread, Lock
+from skimage.filters import threshold_local
 import time
+
+class DominantColor(object):
+
+    def __init__(self, name='thread dominant color'):
+        self.image = None 
+        self.dominant_color = None
+        self.blank_paper = None
+        self.stopped = False
+
+        self.thread = Thread(target=self.update, name=name, args=())
+        self.thread.daemon = True
+
+    def update(self):
+        while not self.stopped:
+            if self.image is None:
+                continue
+            self.dominant_color, self.blank_paper = self.__update_dominant_color(self.image)
+            # print(self.dominant_color)
+            # break
+            time.sleep(60)
+
+    def start(self):
+        self.thread.start()
+
+    def stop(self):
+        self.stopped = True
+
+    def update_image(self, image):
+        self.image = image
+
+    def get_color(self):
+        return self.dominant_color
+
+    def get_blank_paper(self):
+        return self.blank_paper
+
+    def __update_dominant_color(self, image, k=2):
+        blank_paper = np.zeros_like(image).astype(np.uint8)
+        #reshape the image to be a list of pixels
+        image = cv2.resize(image, (128, 128))
+        image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+        #cluster and assign labels to the pixels 
+        clt = KMeans(n_clusters = k)
+        labels = clt.fit_predict(image)
+
+        #count labels to find most popular
+        label_counts = Counter(labels)
+
+        #subset out most popular centroid
+        dominant_color = clt.cluster_centers_[label_counts.most_common(1)[0][0]]
+        dominant_color = list(dominant_color)
+
+        blank_paper[:] = dominant_color
+        
+        return dominant_color, blank_paper
 
 
 class HandRemover(object):
@@ -16,10 +73,15 @@ class HandRemover(object):
         self.upper_YCbCr_values = np.array((255, 173, 133), dtype = "uint8")
 
         self.background = None
+
+        self.dominant_color = DominantColor()
+        self.dominant_color.start()
         
     def process(self, image, is_cropped):
         if is_cropped == False:
             return image
+
+        self.dominant_color.update_image(image)
 
         if self.background is None:
             self.background = image
@@ -33,7 +95,40 @@ class HandRemover(object):
         
         # self.background[]
 
+        self.background = self.__flood_fill(self.background.copy())
+
         return self.background
+
+    def __flood_fill(self, image):
+        test = image
+        test = cv2.Canny(test,250,300)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3, 3))
+        test = cv2.dilate(test, kernel, iterations=3)
+        
+        # Copy the thresholded image.
+        im_floodfill = test.copy()
+
+        # Mask used to flood filling.
+        # Notice the size needs to be 2 pixels than the image.
+        h, w = test.shape[:2]
+        mask = np.zeros((h+2, w+2), np.uint8)
+
+        # Floodfill from point (0, 0)
+        cv2.floodFill(im_floodfill, mask, (0,0), 255)
+
+        # Invert floodfilled image
+        im_floodfill_inv = cv2.bitwise_not(im_floodfill)
+
+        # Combine the two images to get the foreground.
+        im_out = test | im_floodfill_inv
+
+        test2 = image
+        test2 = cv2.bitwise_and(test2, test2, mask=im_out)
+        color = self.dominant_color.get_color()
+        if color is not None:
+            test2[test2[..., 0]==0] = color
+
+        return test2
 
     def __get_hand_mask(self, image):
         HSV_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
